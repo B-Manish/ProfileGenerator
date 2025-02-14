75baba0f8d129daadd0f6f9e2934361acf8eafca
B-Manish <batchumanish@gmail.com>
2025-02-15 00:33:28 +0530

    Printing response

--- deploy.js
+++ deploy.js
@@ -1,16 +1,17 @@
 const axios = require("axios");
 
-
 const apiUrl1 = "https://a7wg8ep9di.execute-api.us-east-1.amazonaws.com/prod/profilegenerator/bsdk.zip";
 const apiUrl2 = "https://a7wg8ep9di.execute-api.us-east-1.amazonaws.com/default/generateurl";
 
 const deployToNetlify = async () => {
     try {
+        console.log("Generating netlify url .......");
         const [response1, response2] = await Promise.all([
-            axios.put(apiUrl1),
+            axios.put(apiUrl1), 
             axios.get(apiUrl2)
         ]);
 
+        console.log(response2.data);
     } catch (error) {
         console.error("Error fetching APIs:", error);
     }
