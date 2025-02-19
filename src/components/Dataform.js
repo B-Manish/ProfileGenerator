94286c4370b4f3fd8e66030177d21539ccdb04fd
B-Manish <batchumanish@gmail.com>
2025-02-19 12:03:21 +0530

    Changed datform component

--- src/components/Dataform.js
+++ src/components/Dataform.js
@@ -5,113 +5,155 @@ import { TextField, Button, Box, Typography } from '@mui/material';
 function Dataform() {
     const { data, setData } = useContext(DataContext);
 
-    // Reusable handleChange function
-    const handleChange = (event, index,item) => {
-        const { name, value } = event.target;
-        // setData((prev) => ({
-        //     ...prev,
-        //     [name]: value,
-        // }));
-
-        setData((prev) => {
-            if(item==="built"){
-                const updatedBuilt = [...prev.built];  
-                updatedBuilt[index] = {                
-                    ...updatedBuilt[index],            
-                    [name]: value,                    
-                };
+    const handleInputChange = (event, key, subKey, index) => {
+        const value = event.target.value;
+        setData((prevData) => {
+          const updatedData = { ...prevData };
     
-                return {
-                    ...prev,
-                    built: updatedBuilt,               
-                };
-
-            }
-            return {
-                ...prev,
-                [name]: value,
-            };
+          if (subKey) {
+            updatedData[key][subKey][index] = value;
+          } else {
+            updatedData[key] = value;
+          }
+    
+          return updatedData;
         });
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-    };
+      };
+    
+      const handleArrayInputChange = (event, key, subKey, index) => {
+        const value = event.target.value;
+        setData((prevData) => {
+          const updatedData = { ...prevData };
+          updatedData[key][subKey][index] = value;
+          return updatedData;
+        });
+      };
 
     return (
-        <Box
-            component="form"
-            onSubmit={handleSubmit}
-            sx={{
-                color: 'white',
-                padding: '20px',
-                maxWidth: '400px',
-                margin: 'auto',
-                display: 'flex',
-                flexDirection: 'column',
-                gap: '16px',
-            }}
-        >
-            <Typography variant="h6">Input Form</Typography>
-
+        <Box>
+        {/* Basic Information */}
+        <TextField
+          fullWidth
+          label="Name"
+          value={data.name}
+          onChange={(e) => handleInputChange(e, 'name')}
+          margin="normal"
+        />
+        <TextField
+          fullWidth
+          label="Description"
+          value={data.desc}
+          onChange={(e) => handleInputChange(e, 'desc')}
+          margin="normal"
+          multiline
+        />
+        <TextField
+          fullWidth
+          label="Brief Description"
+          value={data.briefdesc}
+          onChange={(e) => handleInputChange(e, 'briefdesc')}
+          margin="normal"
+          multiline
+        />
+        <TextField
+          fullWidth
+          label="Email"
+          value={data.mail}
+          onChange={(e) => handleInputChange(e, 'mail')}
+          margin="normal"
+          type="email"
+        />
+  
+        {/* About Me Section */}
+        <Typography variant="h6" gutterBottom>About Me</Typography>
+        {data.aboutme.aboutmedesc.map((desc, index) => (
+          <TextField
+            key={index}
+            fullWidth
+            label={`About Me Description ${index + 1}`}
+            value={desc}
+            onChange={(e) => handleArrayInputChange(e, 'aboutme', 'aboutmedesc', index)}
+            margin="normal"
+            multiline
+          />
+        ))}
+  
+        <Typography variant="h6" gutterBottom>Recent Technologies</Typography>
+        {data.aboutme.recenttechnologies.map((tech, index) => (
+          <TextField
+            key={index}
+            fullWidth
+            label={`Technology ${index + 1}`}
+            value={tech}
+            onChange={(e) => handleArrayInputChange(e, 'aboutme', 'recenttechnologies', index)}
+            margin="normal"
+          />
+        ))}
+  
+        {/* Built Projects */}
+        <Typography variant="h6" gutterBottom>Built Projects</Typography>
+        {data.built.map((project, index) => (
+          <Box key={index} mb={3}>
             <TextField
-                label="Name"
-                variant="outlined"
-                name="name"
-                value={data?.name || ''}
-                onChange={handleChange}
-                fullWidth
+              fullWidth
+              label="Project Name"
+              value={project.name}
+              onChange={(e) => handleArrayInputChange(e, 'built', 'name', index)}
+              margin="normal"
             />
-
-            {/* Description Field */}
             <TextField
-                label="Description"
-                variant="outlined"
-                name="desc"
-                value={data?.desc || ''}
-                onChange={handleChange}
-                fullWidth
+              fullWidth
+              label="Project Description"
+              value={project.desc}
+              onChange={(e) => handleArrayInputChange(e, 'built', 'desc', index)}
+              margin="normal"
+              multiline
             />
-
+            {project.technologies.map((tech, techIndex) => (
+              <TextField
+                key={techIndex}
+                fullWidth
+                label={`Technology ${techIndex + 1}`}
+                value={tech}
+                onChange={(e) => handleArrayInputChange(e, 'built', 'technologies', techIndex)}
+                margin="normal"
+              />
+            ))}
+          </Box>
+        ))}
+  
+        {/* Other Projects */}
+        <Typography variant="h6" gutterBottom>Other Projects</Typography>
+        {data.projects.map((project, index) => (
+          <Box key={index} mb={3}>
             <TextField
-                label="Brief Description"
-                variant="outlined"
-                name="briefdesc"
-                value={data?.briefdesc || ''}
-                onChange={handleChange}
-                fullWidth
+              fullWidth
+              label="Project Name"
+              value={project.name}
+              onChange={(e) => handleArrayInputChange(e, 'projects', 'name', index)}
+              margin="normal"
             />
-
-            Built
-
-
-            {data?.built?.map((item, index) => {
-                return (
-                    <> <TextField
-                        label="Project Name"
-                        variant="outlined"
-                        name="name"
-                        value={item?.name || ''}
-                        onChange={(event) => handleChange(event, index,"built")}
-                        fullWidth
-
-                    /><TextField
-                            label="Project Desciption"
-                            variant="outlined"
-                            name="desc"
-                            value={item?.desc || ''}
-                            onChange={(event) => handleChange(event, index,"built")}
-                            fullWidth
-
-                        /></>
-                );
-            })}
-
-            {/* Submit Button */}
-            <Button type="submit" variant="contained" color="primary" fullWidth>
-                Submit
-            </Button>
-        </Box>
+            <TextField
+              fullWidth
+              label="Project Description"
+              value={project.desc}
+              onChange={(e) => handleArrayInputChange(e, 'projects', 'desc', index)}
+              margin="normal"
+              multiline
+            />
+            {project.technologies.map((tech, techIndex) => (
+              <TextField
+                key={techIndex}
+                fullWidth
+                label={`Technology ${techIndex + 1}`}
+                value={tech}
+                onChange={(e) => handleArrayInputChange(e, 'projects', 'technologies', techIndex)}
+                margin="normal"
+              />
+            ))}
+          </Box>
+        ))}
+      </Box>
     );
 }
 
