d4c296d3ce980a7f557978296aec78c53a4d945b
B-Manish <batchumanish@gmail.com>
2025-02-20 16:46:53 +0530

    removed preview variable from env variables

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Gg from "./components/Gg";
 import Template from "./components/Template";
 import Home from "./components/Home";
 import { DataContext } from "./contexts/DataContext";
+import data from "./data.json";
 
 const customTheme = createTheme({
   breakpoints: {
@@ -20,41 +21,37 @@ const customTheme = createTheme({
 
 
 function App() {
-  const { data } = useContext(DataContext);
-  if (process.env.REACT_APP_BUILD_FOR_PREVIEW === "true") {
-    return <ThemeProvider theme={customTheme}>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Template
-              preview={false}
-              mail={data?.mail}
-              page={
-                <Home
-                  name={data?.name}
-                  desc={data?.desc}
-                  briefdesc={data?.briefdesc}
-                  preview={true}
-                />
-              }
-            />
-          }
-        />
-      </Routes>
-    </ThemeProvider>;
-  }
+
+
+
   return <ThemeProvider theme={customTheme}>
     <Routes>
       <Route
         path="/"
         element={
+          <Template
+            preview={false}
+            mail={data?.mail}
+            page={
+              <Home
+                name={data?.name}
+                desc={data?.desc}
+                briefdesc={data?.briefdesc}
+                preview={true}
+              />
+            }
+          />
+        }
+      />
+      <Route
+        path="/preview"
+        element={
           <Gg />
         }
       />
-
     </Routes>
   </ThemeProvider>;
 }
+
 
 export default App;
